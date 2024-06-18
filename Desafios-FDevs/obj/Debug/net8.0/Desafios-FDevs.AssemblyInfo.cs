ae7ea0aebcedd99bca8b170979b5113308b0400b
felipe.grigolato <fegri@gmail.com>
2024-06-18 14:03:49 -0300

    Readme atualizado

--- Desafios-FDevs/obj/Debug/net8.0/Desafios-FDevs.AssemblyInfo.cs
+++ Desafios-FDevs/obj/Debug/net8.0/Desafios-FDevs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Desafios-FDevs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d84c75924cd2158eff0e5fa970259283057382d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Desafios-FDevs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Desafios-FDevs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Desafios-CSharp
+
+3 Exercicios solicitados pelo mentor Alexandre dentro do curso Back-End na trilha C# Iniciante
